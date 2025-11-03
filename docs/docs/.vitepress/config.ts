883ee91750bb787a1bf9f71a7ad5241f849971f8
sam247 <sampettiford@googlemail.com>
2025-11-03 15:45:16 +0000

    feat: enable VitePress sitemap generation and clean up static files
    - Enable automatic sitemap generation in VitePress config for docs.disclosurely.com
    - Remove old static public/sitemap.xml (replaced by dynamic API route)
    - Update robots.txt to include both sitemap and sitemap-index
    - Remove misplaced SITEMAP.md from docs directory

--- docs/docs/.vitepress/config.ts
+++ docs/docs/.vitepress/config.ts
@@ -1,4 +1,9 @@
 import { defineConfig } from 'vitepress'
 
 // https://vitepress.vuejs.org/config/app-configs
-export default defineConfig({})
+export default defineConfig({
+  // Enable automatic sitemap generation
+  sitemap: {
+    hostname: 'https://docs.disclosurely.com'
+  }
+})
