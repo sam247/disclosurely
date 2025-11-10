e76380ee4b15e64649f17ff4adb0f7ee26da4d74
sam247 <sampettiford@googlemail.com>
2025-11-10 16:27:29 +0000

    Fix syntax errors in auth button components

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -139,9 +139,7 @@ const LoginForm = () => {
         loading={googleLoading}
         loadingText={t('auth.signin.signingInWithGoogle')}
       >
-        {
-          <>
-            <svg className="w-4 h-4 mr-2" viewBox="0 0 24 24">
+        <svg className="w-4 h-4 mr-2" viewBox="0 0 24 24">
               <path
                 fill="currentColor"
                 d="M22.56 12.25c0-.78-.07-1.53-.2-2.25H12v4.26h5.92c-.26 1.37-1.04 2.53-2.21 3.31v2.77h3.57c2.08-1.92 3.28-4.74 3.28-8.09z"
