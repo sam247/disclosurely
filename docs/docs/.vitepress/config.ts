01823d761ddfe2f452822ba0b88d9f56931dc8bd
sam247 <sampettiford@googlemail.com>
2025-12-06 17:49:53 +0000

    fix: Remove VitePress base path to fix double /docs/ in internal links

--- docs/docs/.vitepress/config.ts
+++ docs/docs/.vitepress/config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.vuejs.org/config/app-configs
 export default defineConfig({
-  base: '/docs/',
   // Sitemap generation disabled - all docs pages are included in main sitemap
   // at disclosurely.com/sitemap.xml for unified SEO authority
 })
