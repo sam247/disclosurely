d2aa2252b0f9362c8f0ff7d65bcad0aba67b1fb3
sam247 <sampettiford@googlemail.com>
2025-12-06 17:19:30 +0000

    fix: Set VitePress base path to /docs/ for correct routing

--- docs/docs/.vitepress/config.ts
+++ docs/docs/.vitepress/config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.vuejs.org/config/app-configs
 export default defineConfig({
+  base: '/docs/',
   // Sitemap generation disabled - all docs pages are included in main sitemap
   // at disclosurely.com/sitemap.xml for unified SEO authority
 })
